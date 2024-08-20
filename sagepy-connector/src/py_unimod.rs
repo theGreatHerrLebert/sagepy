9345093279b2e3a0ba6dff42f17285d81c74f573
theGreatHerrLebert <davidteschner@googlemail.com>
2024-08-20 12:48:16 +0200

    adding unimod annotations to sagepy

--- sagepy-connector/src/py_unimod.rs
+++ sagepy-connector/src/py_unimod.rs
@@ -1,14 +1,20 @@
 use std::collections::HashMap;
 use pyo3::prelude::*;
-use unimod::unimod::unimod_modifications_mass_numerical;
+use unimod::unimod::{unimod_modifications_mass_numerical, unimod_modifications_mass};
 
 #[pyfunction]
 fn unimod_modification_to_mass_numerical() -> HashMap<u32, f64> {
     unimod_modifications_mass_numerical()
 }
 
+#[pyfunction]
+fn unimod_modification_to_mass() -> HashMap<&'static str, f64> {
+    unimod_modifications_mass()
+}
+
 #[pymodule]
 pub fn unimodifications(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(unimod_modification_to_mass_numerical, m)?)?;
+    m.add_function(wrap_pyfunction!(unimod_modification_to_mass, m)?)?;
     Ok(())
 }
