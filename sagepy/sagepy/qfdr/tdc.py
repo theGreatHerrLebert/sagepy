c0c68300705bcc2e264a5ef31f4bbfc4ac98e5d7
theGreatHerrLebert <davidteschner@googlemail.com>
2024-09-06 09:09:51 +0200

    fixing crash on None fragments observed when annotate_matches = False

--- sagepy/sagepy/qfdr/tdc.py
+++ sagepy/sagepy/qfdr/tdc.py
@@ -54,7 +54,6 @@ def target_decoy_competition_pandas(
         df: pd.DataFrame,
         method: str = "peptide_psm_peptide",
         score: Optional[str] = None,
-        merge: bool = False,
 ) -> pd.DataFrame:
     """ Perform target-decoy competition on a pandas DataFrame.
 
@@ -62,7 +61,6 @@ def target_decoy_competition_pandas(
         df: a pandas DataFrame
         method: the method to use, allowed values are: psm, peptide_psm_only, peptide_peptide_only, peptide_psm_peptide
         score: the target column name (optional)
-        merge: whether to merge the q-values with the original DataFrame
 
     Returns:
         a pandas DataFrame with q-values
@@ -73,6 +71,7 @@ def target_decoy_competition_pandas(
     for col in required_columns:
         assert col in df.columns, f"{col} column not found"
 
+    # Ensure score column is present
     score_col = score if score else 'score'
     assert score_col in df.columns, f"{score_col} column not found"
 
@@ -90,22 +89,8 @@ def target_decoy_competition_pandas(
         'spec_idx': spec_idx,
         'match_idx': match_idx,
         'decoy': target,
-        're_score': scores,
+        'score': scores,
         'q_value': q_values
-    }).sort_values(by=['q_value'])
-
-    # If merging results with the original DataFrame
-    if merge:
-        df_no_score = df.drop(columns=[score_col], errors='ignore')
-
-        df_tdc = pd.merge(
-            df_tdc,
-            df_no_score,
-            on=["match_idx", "spec_idx", "decoy"],
-            how="left",
-            suffixes=('', '_drop')
-        )
-
-        df_tdc = df_tdc.loc[:, ~df_tdc.columns.str.endswith('_drop')]
+    }).sort_values(by=['q_value'], ascending=True)
 
     return df_tdc
