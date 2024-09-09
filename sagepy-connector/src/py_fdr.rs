5f768a684d65702a6741cd38d24f1341260486b6
theGreatHerrLebert <davidteschner@googlemail.com>
2024-09-09 16:49:51 +0200

    adding py_fdr picked protein and picked peptide

--- sagepy-connector/src/py_fdr.rs
+++ sagepy-connector/src/py_fdr.rs
@@ -1,7 +1,9 @@
 use pyo3::prelude::*;
-use sage_core::fdr::{Competition};
-use sage_core::database::PeptideIx;
-use crate::py_database::{PyPeptideIx};
+use sage_core::fdr::{Competition, picked_peptide, picked_protein};
+use sage_core::database::{IndexedDatabase, PeptideIx};
+use sage_core::scoring::Feature;
+use crate::py_database::{PyIndexedDatabase, PyPeptideIx};
+use crate::py_scoring::PyFeature;
 
 #[pyclass]
 // TODO: Check if it makes sense to tie this to PeptideIx
@@ -43,8 +45,24 @@ impl PyCompetitionPeptideIx {
     }
 }
 
+#[pyfunction]
+pub fn py_picked_peptide(feature_collection: Vec<PyFeature>, indexed_database: &PyIndexedDatabase) -> Vec<PyFeature> {
+    let mut inner_collection: Vec<Feature> = feature_collection.iter().map(|feature| feature.inner.clone()).collect();
+    let _ = picked_peptide(&indexed_database.inner, &mut inner_collection);
+    inner_collection.into_iter().map(|feature| PyFeature { inner: feature }).collect()
+}
+
+#[pyfunction]
+pub fn py_picked_protein(feature_collection: Vec<PyFeature>, indexed_database: &PyIndexedDatabase) -> Vec<PyFeature> {
+    let mut inner_collection: Vec<Feature> = feature_collection.iter().map(|feature| feature.inner.clone()).collect();
+    let _ = picked_protein(&indexed_database.inner, &mut inner_collection);
+    inner_collection.into_iter().map(|feature| PyFeature { inner: feature }).collect()
+}
+
 #[pymodule]
 pub fn fdr(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PyCompetitionPeptideIx>()?;
+    m.add_function(wrap_pyfunction!(py_picked_peptide, m)?)?;
+    m.add_function(wrap_pyfunction!(py_picked_protein, m)?)?;
     Ok(())
 }
