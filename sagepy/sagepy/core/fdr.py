62513b24e4bb775e87414a44a9c82dcbbe928b5c
theGreatHerrLebert <davidteschner@googlemail.com>
2024-11-24 19:45:43 +0100

    trying multi input for sage FDR

--- sagepy/sagepy/core/fdr.py
+++ sagepy/sagepy/core/fdr.py
@@ -1,4 +1,4 @@
-from typing import Optional, List
+from typing import Optional, List, Union, Dict
 from sagepy.core.scoring import Psm
 from sagepy.core import IndexedDatabase, Feature
 from sagepy.core.database import PeptideIx
@@ -61,15 +61,25 @@ def sage_fdr(feature_collection: List[Feature], indexed_db: IndexedDatabase, use
         use_hyper_score
     )
 
-def sage_fdr_psm(feature_collection: List[Psm], indexed_db: IndexedDatabase, use_hyper_score: bool = True):
+def sage_fdr_psm(feature_collection: Union[List[Psm], Dict[str, List[Psm]]], indexed_db: IndexedDatabase, use_hyper_score: bool = True):
     """ Perform SAGE FDR on all levels (spectrum, peptide, protein), calculates q-values and PEPs for a given feature collection.
     Args:
         feature_collection: a list of features
         indexed_db: an indexed database
         use_hyper_score: whether to use hyper score or discriminant score for q-value calculation
     """
+
+    f_collection = []
+
+    if isinstance(feature_collection, dict):
+        for _, values in feature_collection.items():
+            f_collection.extend(values)
+
+    else:
+        f_collection = feature_collection
+
     psc.py_sage_fdr_psm(
-        [feature.get_py_ptr() for feature in feature_collection],
+        [feature.get_py_ptr() for feature in f_collection],
         indexed_db.get_py_ptr(),
         use_hyper_score
     )
