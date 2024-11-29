124d955ee7962e698b0339b6f5485f587e5c0b1d
theGreatHerrLebert <davidteschner@googlemail.com>
2024-11-29 12:22:02 +0100

    adding mod sequence vs sequence

--- sagepy/sagepy/utility.py
+++ sagepy/sagepy/utility.py
@@ -467,6 +467,7 @@ def psm_collection_to_pandas(psm_collection: Union[List[Psm], Dict[str, List[Psm
 
     # extract the peptide sequences and spectrum indices
     sequence = psc.get_psm_sequences_par([psm.get_py_ptr() for psm in psms], num_threads=num_threads)
+    sequence_modified = psc.get_psm_sequences_modified_par([psm.get_py_ptr() for psm in psms], num_threads=num_threads)
     sequence_decoy = psc.get_psm_sequences_decoy_par([psm.get_py_ptr() for psm in psms], num_threads=num_threads)
     spec_idx = psc.get_psm_spec_idx_par([psm.get_py_ptr() for psm in psms], num_threads=num_threads)
     proteins = psc.get_psm_proteins_par([psm.get_py_ptr() for psm in psms], num_threads=num_threads)
@@ -485,7 +486,8 @@ def psm_collection_to_pandas(psm_collection: Union[List[Psm], Dict[str, List[Psm
     PSM_pandas.insert(1, "match_idx", sequence)
     PSM_pandas.insert(2,"match_identity_candidates", proteins)
     PSM_pandas.insert(3, "sequence", sequence)
-    PSM_pandas.insert(4, "sequence_decoy", sequence_decoy)
-    PSM_pandas.insert(5, "proteins", proteins)
+    PSM_pandas.insert(4, "sequence_modified", sequence_modified)
+    PSM_pandas.insert(5, "sequence_decoy", sequence_decoy)
+    PSM_pandas.insert(6, "proteins", proteins)
 
     return PSM_pandas
