606b5cb4d3c7bb8f8f04c288429e0614862da347
theGreatHerrLebert <davidteschner@googlemail.com>
2025-01-09 14:27:09 +0100

    adding RT model

--- sagepy-connector/src/lib.rs
+++ sagepy-connector/src/lib.rs
@@ -21,7 +21,6 @@ pub mod py_intensity;
 pub mod py_retention_model;
 pub mod py_retention_alignment;
 pub mod py_mobility_model;
-
 #[pymodule]
 fn sagepy_connector(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
 
@@ -41,6 +40,7 @@ fn sagepy_connector(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_wrapped(wrap_pymodule!(py_unimod::py_unimod))?;
     m.add_wrapped(wrap_pymodule!(py_utility::py_utility))?;
     m.add_wrapped(wrap_pymodule!(py_intensity::py_intensity))?;
+    m.add_wrapped(wrap_pymodule!(py_retention_alignment::py_retention_alignment))?;
 
     Ok(())
 }
